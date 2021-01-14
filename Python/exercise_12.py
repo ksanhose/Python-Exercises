9a0102b8fce053a4cc2c1ae716adf39f0c0f7935
ksanhose <35140556+ksanhose@users.noreply.github.com>
2021-01-14 22:49:46 +0200

    Create exercise_12.py

---
+++ Python/exercise_12.py
@@ -0,0 +1,30 @@
+import textwrap
+
+
+def get_mirrored_ascii_text(text, uppercase=True):
+    if uppercase:
+        text = text.upper()
+
+    mirrored_chars = []
+    for char in text:
+
+        # The printable ascii range runs from 32 to 126
+        if 32 <= 128 - ord(char) <= 126:
+            mirrored_chars.append(chr(128 - ord(char)))
+
+    mirrored_ascii_text = ''.join(mirrored_chars[::-1])
+
+    return mirrored_ascii_text
+
+
+if __name__ == '__main__':
+    text = '''Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 
+    to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem 
+    Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    '''
+
+    mirrored_ascii_text = get_mirrored_ascii_text(text, uppercase=True)
+
+    print('\n'.join(textwrap.wrap(mirrored_ascii_text, 64)))
